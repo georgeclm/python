1f31c6c6513e16317659910d79e4bba015bad519
George <cavidjaja@gmail.com>
2021-04-20 14:07:14 +0700

    data

--- Week13.py
+++ Week13.py
@@ -1,12 +1,15 @@
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from numpy import genfromtxt
+np.set_printoptions(precision=5)
 data = genfromtxt('even_featurescaling.csv', delimiter=',')
 
-
+print('Before Scaling:')
 print(data)
+print('\n')
 # Use feature_range named argument to scale it to a different scale. e.g
 scaler = MinMaxScaler(feature_range=(0, 10))
 # scaler = MinMaxScaler()
 scaled = scaler.fit_transform(data)
+print('After Scaled:')
 print(scaled)
---
+++ week14.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+df = pd.read_csv('gold_karat.csv')
+
+df.head()
